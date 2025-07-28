b1a9cdc5fe5f763661caa1e4b0e6a57b0e201f4f
holistika700 <arifmuhaimin2306@gmail.com>
2025-07-28 07:38:07 +0000

    Add SQLALCHEMY_DATABASE_URI config

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ class Base(DeclarativeBase):
 
 # Initialize Flask app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///local.db'
+
 app.secret_key = os.environ.get("SESSION_SECRET")
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1) # needed for url_for to generate with https
 
